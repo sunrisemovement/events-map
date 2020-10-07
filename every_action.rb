8b84d99a6d55824de59502b2ce4e656824254a8d
Brian Hicks <brian@brianthicks.com>
2020-10-07 08:04:02 -0500

    upgrade every_action.rb to strict

--- every_action.rb
+++ every_action.rb
@@ -1,15 +1,26 @@
-# typed: true
-class EveryActionClient
-  attr_reader :api_key, :username
+# typed: strict
 
+class EveryActionClient
+  extend T::Sig
+
+  sig { returns(String) }
+  attr_reader :api_key
+
+  sig { returns(String) }
+  attr_reader :username
+
+  sig { params(api_key: String, username: String).void }
   def initialize(api_key, username: "sunrise-movement")
     # Currently default to "sunrise-movement" for the EA username, because it
     # seems like this will be constant across keys, but may need to update if
     # we add more keys later.
     @username = username
     @api_key = api_key
+
+    @events = T.let([], T::Array[EveryActionEvent])
   end
 
+  sig { returns(T::Array[EveryActionEvent]) }
   def events_request
     # Request upcoming events from the EveryAction API, iteratively handling pagination
     url = T.let("https://api.securevan.com/v4/events?startingAfter=#{Date.today-1}&$expand=onlineforms,locations,codes,shifts,roles,notes", T.nilable(String))
@@ -25,15 +36,22 @@ class EveryActionClient
       res += resp["items"]
       url = resp["nextPageLink"]
     end
-    res
+
+    res.map { |e| EveryActionEvent.new(e) }
   end
 
+  sig { returns(T::Array[EveryActionEvent]) }
   def events
     # Make events requests (possibly recursively), then wrap each response in a
     # helper object for filtering and formatting
-    @events ||= events_request.map { |e| EveryActionEvent.new(e) }
+    if @events.empty?
+      @events = events_request
+    end
+
+    @events
   end
 
+  sig { returns(T::Array[T::Hash[Symbol, T.untyped]]) }
   def map_entries
     # Filter events and get their map entries.
     events.select(&:should_appear_on_map?).map(&:map_entry)
@@ -41,13 +59,18 @@ class EveryActionClient
 end
 
 class EveryActionEvent
+  extend T::Sig
+
+  sig { returns(T::Hash[String, T.untyped]) }
   attr_reader :data
 
+  sig { params(data: T::Hash[String, T.untyped]).void }
   def initialize(data)
     # Initialize with the raw API response
     @data = data
   end
 
+  sig { returns(T::Boolean) }
   def should_appear_on_map?
     # Events appear if they're associated with a published online form and if
     # they're listed as active (and also if they're not excluded by event type,
@@ -55,11 +78,13 @@ class EveryActionEvent
     registration_link.present? && data['isActive']
   end
 
+  sig { returns(T.nilable(String)) }
   def event_type
     # Get the event type from the response (w/ null-safety)
     (data["eventType"] || {})["name"]
   end
 
+  sig { returns(T::Hash[Symbol, T.untyped]) }
   def map_entry
     # The main method of this class -- this is the data we surface in the JSON.
     {
@@ -83,6 +108,7 @@ class EveryActionEvent
     }
   end
 
+  sig { returns(T::Array[EveryActionOnlineForm]) }
   def online_forms
     # Select online forms that are currently published.
     (data['onlineForms'] || []).
@@ -90,42 +116,54 @@ class EveryActionEvent
       select(&:published?)
   end
 
+  sig { returns(T.nilable(String)) }
   def registration_link
     online_forms.first.try(:url)
   end
 
+  sig { returns(T.nilable(String)) }
   def featured_image_url
     online_forms.first.try(:featured_image_url)
   end
 
+  sig { returns(T::Array[T.untyped]) }
   def locations
     data["locations"] || []
   end
 
+  sig { returns(T::Hash[T.untyped, T.untyped]) }
   def location
     locations.first || {}
   end
 
+  sig { returns(T::Hash[T.untyped, T.untyped]) }
   def address
     location['address'] || {}
   end
 
+  sig { returns(T.nilable(Float)) }
   def latitude
     (address["geoLocation"] || {})["lat"]
   end
 
+  sig { returns(T.nilable(Float)) }
   def longitude
     (address["geoLocation"] || {})["lon"]
   end
 end
 
 class EveryActionOnlineForm
+  extend T::Sig
+  
+  sig { returns(T::Hash[String, T.untyped]) }
   attr_reader :data
 
+  sig { params(data: T::Hash[String, T.untyped]).void }
   def initialize(data)
     @data = data
   end
 
+  sig { returns(T::Hash[String, T.untyped]) }
   def json_entry
     data.merge({
       "bannerImagePath" => featured_image_url,
@@ -133,23 +171,30 @@ class EveryActionOnlineForm
     })
   end
 
+  sig { returns(T::Boolean) }
   def published?
     data["status"] == "Published"
   end
 
+  sig { returns(T.nilable(String)) }
   def url
     data["url"]
   end
 
+  sig { returns(String) }
   def form_def_url
     # It's not documented in their official API, but @Jared discovered there is
     # a JSON endpoint containing form definition information not included in
     # the limited onlineActionForms response, which in particular includes a
     # featured image URL. This URL can be constructed as follows:
-    url.sub("https://secure.everyaction.com",
-            "https://secure.everyaction.com/v2/Forms")
+    url.try(
+      :sub,
+      "https://secure.everyaction.com",
+      "https://secure.everyaction.com/v2/Forms"
+    )
   end
 
+  sig { returns(T::Hash[T.untyped, T.untyped]) }
   def form_def_response
     HTTParty.get(form_def_url, headers: {
       "Content-Type" => "application/json"
@@ -158,25 +203,33 @@ class EveryActionOnlineForm
     {} # Return empty hash in the event this stops working in the future
   end
 
+  sig { returns(T.untyped) }
   def form_def
-    @form_def ||= (form_def_response || {})
+    @form_def ||= T.let(
+      form_def_response,
+      T.nilable(T::Hash[T.untyped, T.untyped]),
+    )
   end
 
+  sig { returns(T.nilable(String)) }
   def featured_image_url
     form_def["bannerImagePath"]
   end
 
+  sig { returns(T::Array[T.untyped]) }
   def form_elements
     form_def["form_elements"] || []
   end
 
+  sig { returns(T.untyped) }
   def header_element
     # The form definition object includes a header element that looks a lot
     # like a description. This can be used in place of an explicitly given text
     # description for the event.
-    form_elements.detect{|el| el["name"] == "HeaderHtml" && el["type"] == "markup" }
+    form_elements.detect{ |el| el["name"] == "HeaderHtml" && el["type"] == "markup" }
   end
 
+  sig { returns(T.nilable(String)) }
   def description
     # Return the description described above (as an HTML string)
     if header_element
