36a9bd1536ebba31bd34cf9242b7c9f24917a8dd
Brian Hicks <brian@brianthicks.com>
2020-10-07 06:09:05 -0500

    upgrade action_network.rb to strict

--- action_network.rb
+++ action_network.rb
@@ -1,4 +1,4 @@
-# typed: true
+# typed: strict
 require 'httparty'
 require 'dotenv'
 
@@ -15,37 +15,48 @@ Dotenv.load
 # Wrapper class around the ActionNetwork Event JSON object that provides helper
 # methods for transforming it into JSON for the event map
 class ActionNetworkEvent
+  extend T::Sig
+
+  sig { returns(T.untyped) }
   attr_reader :data
 
+  sig { params(data: T.untyped).void }
   def initialize(data)
     @data = data
   end
 
+  sig { returns(Time) }
   def start_time
     Time.parse(data['start_date'])
   end
 
+  sig { returns(T::Boolean) }
   def is_public?
     data['visibility'] == 'public'
   end
 
   # Events should appear if they're public and if they haven't already happened
+  sig { returns(T.nilable(T::Boolean)) } # TODO
   def should_appear?
     !data['start_date'].nil? && is_public? && Date.parse(data['start_date']) >= Date.today
   end
 
+  sig { returns(T::Hash[String, T.untyped]) }
   def location
-    data['location']
+    data['location'] || {}
   end
 
+  sig { returns(T.nilable(Float)) }
   def latitude
     location['location']['latitude']
   end
 
+  sig { returns(T.nilable(Float)) }
   def longitude
     location['location']['longitude']
   end
 
+  sig { returns(T.nilable(String)) }
   def description
     if desc = data['description']
       desc.gsub!(/<\/?[^>]*>/, "")
@@ -57,6 +68,7 @@ class ActionNetworkEvent
     end
   end
 
+  sig {returns(T::Hash[Symbol, T.untyped])}
   def map_entry
     {
       city: location['locality'],
@@ -78,30 +90,39 @@ end
 # Wrapper class around the actual request to the ActionNetwork API. Uses the
 # 'httparty' library.
 class ActionNetworkRequest
+  extend T::Sig
+
   include HTTParty
   base_uri 'https://actionnetwork.org'
 
+  sig { params(page: Integer).void }
   def initialize(page)
     @page = page
-    @options = {
-      query: {
-        page: page,
-        per_page: 25
+    @options = T.let(
+      {
+        query: {
+          page: page,
+          per_page: 25
+        },
+        headers: {
+          'OSDI-API-Token' => ENV['ACTION_NETWORK_API_KEY']
+        }
       },
-      headers: {
-        'OSDI-API-Token' => ENV['ACTION_NETWORK_API_KEY']
-      }
-    }
+      T.untyped # TODO
+    )
   end
 
+  sig { returns(T.untyped) }
   def response
-    @response ||= self.class.get("/api/v2/events", @options)
+    @response ||= T.let(self.class.get("/api/v2/events", @options), T.untyped) # TODO
   end
 
+  sig { returns(T::Boolean) }
   def last_page?
     @page >= response['total_pages']
   end
 
+  sig { returns(T.untyped) }
   def results
     response["_embedded"]["osdi:events"].map { |e| ActionNetworkEvent.new(e) }
   end
@@ -110,6 +131,9 @@ end
 # Wrapper class around ActionNetwork overall. This makes requests to the
 # /events?page=X endpoint until we reach the last page.
 class ActionNetwork
+  extend T::Sig
+
+  sig { params(max_pages: T.untyped).returns(T::Array[T.untyped]) }
   def self.events(max_pages=100)
     results = []
     max_pages.times do |page|
@@ -120,6 +144,7 @@ class ActionNetwork
     results.select(&:should_appear?)
   end
 
+  sig { returns(T::Array[T.untyped]) }
   def self.map_entries
     self.events.map(&:map_entry)
   end
